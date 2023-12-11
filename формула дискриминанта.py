0e429a0f91297ac731c7d2b38b8bd538939eb608
qurtochka <148446840+qurtochka@users.noreply.github.com>
2023-12-11 10:57:21 +0300

    Update формула дискриминанта.py

--- формула дискриминанта.py
+++ формула дискриминанта.py
@@ -1,14 +1,22 @@
 import math
-a = float(input('Введите число а'))
-b = float(input('Введите число b'))
-c = float(input('Введите число c'))
-D = b**2 - 4*a*c
-if D == 0:
-    x = -b / (2*a)
-    print (x)
-elif D < 0:
-    print ('Корней нет')
-elif D > 0:
-    x1 =  (-b + math.sqrt(D))/2*a
-    x2 =  (-b - math.sqrt(D))/2*a
-    print(x1,x2)
+while True:
+    try:
+        a = int(input('Введите число а'))
+        b = int(input('Введите число b'))  
+        c = int(input('Введите число c'))
+    except ValueError:
+        print ('неправильный тип данных')
+    else:
+        D = (b**2) - (4*a*c)
+        if D == 0:
+            x = -b / (2*a)
+            print (x)
+        elif D < 0:
+            print ('Корней нет')
+            break
+        elif D > 0:
+            x1 =  (-b + math.sqrt(D))/(2*a)
+            x2 =  (-b - math.sqrt(D))/(2*a)
+            print(x1,x2)
+            break
+
